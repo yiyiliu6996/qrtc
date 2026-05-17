acfa5de3bf2ec9f8b1e1e969b36590ad8e72839c
Yiyi <yiyiliu6996@gmail.com>
2026-05-17 18:44:06 +0700

    Update qrtc_v2.py

--- qrtc_v2.py
+++ qrtc_v2.py
@@ -293,7 +293,7 @@ BANK_ALIAS: dict[str, str] = {
     # MSB
     "msb":"msb","maritimebank":"msb","hanghaibank":"msb","hanghai":"msb",
     # LPBank
-    449","lpb":"970449","locphat":"970449","lienphat":"970449",
+    "lpbank":"970449","lpb":"970449","locphat":"970449","lienphat":"970449",
     # OCB
     "ocb":"ocb","phuongdong":"ocb","orientcommercial":"ocb",
     # SeABank
